a5eca36380e0662a607a083c781b2d2e435801ea
Sushant Chanmanwar <sushant.chanmanwar@gmail.com>
2020-02-09 23:33:12 +1100

    Added auth-service with basic username-password based authentication

---
+++ auth-service/src/main/java/com/scshop/security/authservice/controller/AuthController.java
@@ -0,0 +1,8 @@
+package com.scshop.security.authservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/login")
+public class AuthController {
+
+}
---
+++ auth-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER_IDENTITY(id, username, password) 
+VALUES('22335d51265a4e01ad4d306be659a48f', 'sushant1', 'sushant');
+
+INSERT INTO USER_IDENTITY(id, username, password) 
+VALUES('32335d51265a4e01ad4d306be659a48f', 'sushant2', 'sushant');
