6b8ee87d49bd92f62483e5f8b94aae218afbd3bf
Sushant Chanmanwar <sushant.chanmanwar@gmail.com>
2020-02-10 21:57:05 +1100

    Congigured JWT token based authentication

--- auth-service/src/main/java/com/scshop/security/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/scshop/security/authservice/controller/AuthController.java
@@ -1,8 +1,58 @@
 package com.scshop.security.authservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/login")
+import com.scshop.security.authservice.config.service.UserDetailsServiceImpl;
+import com.scshop.security.authservice.jwt.JwtConfig;
+import com.scshop.security.authservice.model.AuthenticationRequest;
+import com.scshop.security.authservice.model.AuthenticationResponse;
+
+@RestController
 public class AuthController {
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtConfig jwtConfig;
+
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtConfig.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(token));
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+	}
+	
+	@RequestMapping(value = "/test")
+	public String test(){
+		return "Tesk OK!";
+	}
+	
 }
