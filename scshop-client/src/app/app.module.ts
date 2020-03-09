87e5562ad4ef2e620d60c210459c6fbbae2e7b83
Sushant Chanmanwar <sushant.chanmanwar@gmail.com>
2020-03-09 19:39:31 +1100

    Added some UI code-nothing significant yet

--- scshop-client/src/app/app.module.ts
+++ scshop-client/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
