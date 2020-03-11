963b4a78b2821b4645dd3be60a3c77a23b4a9d6e
Sushant Chanmanwar <sushant.chanmanwar@gmail.com>
2020-03-11 23:06:47 +1100

    Updated cart screen

---
+++ scshop-client/src/app/cart/cart-price-details.model.ts
@@ -0,0 +1,28 @@
+export class CartPriceDetails {
+    
+
+    private _totalPrice: number;
+    private _delivery: number = 20;
+
+    constructor(public price: number = 0){
+
+    }
+
+    public get delivery(){
+        if(this.price == 0){
+             return 0;
+        }
+        this._delivery = 20;
+        return this._delivery;
+    }
+
+    public get totalPrice(){
+       if(this.price == 0){
+            return 0;
+        }
+        return this.price + this.delivery;
+    }
+
+    
+
+}
--- scshop-client/src/app/cart/cart.component.ts
+++ scshop-client/src/app/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../common/product.model';
 import { CartItem } from './cart-item/cart-item.model';
+import { CartPriceDetails } from './cart-price-details.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -9,11 +11,21 @@ import { CartItem } from './cart-item/cart-item.model';
 })
 export class CartComponent implements OnInit {
 
-  cartItems: Array<CartItem> = [new CartItem(), new CartItem()];
+  cartPrice: CartPriceDetails = new CartPriceDetails(0);
+  cartItems: Array<CartItem> = [new CartItem(), new CartItem(), new CartItem(), new CartItem(), new CartItem(), new CartItem(), new CartItem()];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router:Router) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.cartItems.forEach(element => {
+      this.cartPrice.price = this.cartPrice.price + (element.price * element.quantity); 
+    });
+  }
+
+
+  onPlaceOrder(){
+    this.router.navigate(['order']);
+  }
 }
