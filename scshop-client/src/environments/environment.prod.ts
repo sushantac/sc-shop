fdb212855ba8e22bd6fdb1f0a8e4adb89b2e996a
Sushant Chanmanwar <sushant.chanmanwar@gmail.com>
2020-04-03 22:36:57 +1100

    Updated for keycloak settings

--- scshop-client/src/environments/environment.prod.ts
+++ scshop-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: "http://sushantc.com:8000",
+  serverUrl: "http://35.189.38.162:8000",
   authUrl: "http://sushantc.com:7999"
   //serverUrl: "http://localhost:8000"
 };
