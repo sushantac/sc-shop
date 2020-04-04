16705ffd7c0460d03148a93658dc9366ad29e229
Sushant Chanmanwar <sushant.chanmanwar@gmail.com>
2020-04-04 12:42:08 +1100

    Removed old auth-service as now replaced it by keycloak

--- auth-service/src/main/java/com/scshop/application/common/enums/Country.java
+++
@@ -1,5 +0,0 @@
-package com.scshop.application.common.enums;
-
-public enum Country {
-	INDIA, AUSTRALIA, USA, NEW_ZEALAND, CHINA 
-}
--- auth-service/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO USER_IDENTITY(id, username, password, first_name, last_name, birth_date, gender) 
-VALUES('22335d51265a4e01ad4d306be659a48f', 'sushant1', 'sushant', 'Sushant1', 'Chanmanwar', '19800101', 'Male');
-
-INSERT INTO USER_IDENTITY(id, username, password, first_name, last_name, birth_date, gender) 
-VALUES('32335d51265a4e01ad4d306be659a48f', 'sushant2', 'sushant', 'Sushant2', 'Chanmanwar', '19800101', 'Male');
